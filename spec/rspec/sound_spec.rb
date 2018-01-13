4acf03f19b86ca53a8a2c6305fe3ed61cd7f3a2c
Junichi Kobayashi <nnujabok@gmail.com>
2018-01-13 14:17:11 +0900

    Remove failing spec

--- spec/rspec/sound_spec.rb
+++ spec/rspec/sound_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RSpec::Sound do
   it 'has a version number' do
     expect(RSpec::Sound::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
